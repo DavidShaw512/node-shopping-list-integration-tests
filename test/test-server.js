9eb4f2fa6bf977698682abcd07a53786ddcb9656
Admin <admin@Admins-MacBook-7.local>
2019-02-06 22:00:25 -0600

    Tests running, RECIPE PUT returning error

--- test/test-server.js
+++ test/test-server.js
@@ -150,3 +150,88 @@ describe("Shopping List", function() {
     );
   });
 });
+
+describe('Recipes', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  // Recipes GET test:
+  it('should display recipes on GET', function() {
+    return chai
+      .request(app)
+      .get("/recipes")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("array");
+        expect(res.body.length).to.be.at.least(1);
+        const requiredFields = ["name", "ingredients", "id"];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a("object");
+          expect(item).to.include.keys(requiredFields);
+        })
+      });
+  });
+
+  // Recipes POST test:
+  it('should post a new recipe on POST', function() {
+    const newRecipe = {
+      name: "cereal",
+      ingredients: ["milk", "cereal"]
+    };
+    return chai
+      .request(app)
+      .post("/recipes")
+      .send(newRecipe)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.include.keys("name", "ingredients", "id");
+        expect(res.body).to.not.equal(null);
+      })
+  })
+
+  // Recipes PUT test:
+  it('should update a recipe on PUT', function() {
+    const updatedRecipe = {
+      name: "cheerios",
+      ingredients: "cheerios"
+    };
+    return (chai
+      .request(app)
+      .get("/recipes")
+      .then(function(res) {
+        updatedRecipe.id = res.body[0].id;
+        return chai
+          .request(app)
+          .put(`/recipes/${updatedRecipe.id}`)
+          .send(updatedRecipe)
+      })
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+      })
+    );
+  });
+
+  // Recipes DELETE test:
+  it('should delete a recipe on DELETE', function() {
+    return chai
+        .request(app)
+        .get("/recipes")
+        .then(function(res) {
+          return chai.request(app).delete(`/recipes/${res.body[0].id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    
+  });
+});
